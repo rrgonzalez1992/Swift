9a16f1505f80fc27e99a7617e5a334d339ceea72
Ramón Ramírez González <rrgonzalez1992@gmail.com>
2015-11-18 13:49:42 +0100

    Probando Travis

---
+++ prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by Juan Antonio Parrado Guerrero on 18/11/15.
+//  Copyright © 2015 Aleteo Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
